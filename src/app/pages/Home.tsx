c256da46e324d4e1c33d04d14289068f529e6a5e
garizola <99741841+garizola@users.noreply.github.com>
2023-01-25 15:16:31 -0500

    changed title

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -53,7 +53,7 @@ export default function Home({ setSelectedIndex }: Props) {
               justifyContent={{ xs: "center", sm: "flex-start" }}
             >
               <Typography variant="subtitle1" gutterBottom>
-                Front-End Software Developer
+                Software Developer
                 
               </Typography>
             </Grid>
