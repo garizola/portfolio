9ad9d0355dabff8c860d63ae6f56c895b8d23e4b
garizola <99741841+garizola@users.noreply.github.com>
2023-06-05 20:26:20 -0400

    under construction

---
+++ src/ComingSoon.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../src/comingSoon.css';
+
+
+function ComingSoon() {
+  return (
+    
+       <div className="content">
+    <h2>UNDER<span>&nbsp;</span>CONSTRUCTION</h2>
+    <h2>UNDER<span>&nbsp;</span>CONSTRUCTION</h2>
+  </div>
+        
+        
+   
+  )
+}
+
+export default ComingSoon
