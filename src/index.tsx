25de52a9a4fc9f2b3849aad9fd937986235aaabc
garizola <99741841+garizola@users.noreply.github.com>
2023-06-05 20:27:44 -0400

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/layout/App";
+// import App from "./app/layout/App";
 import ComingSoon from "./ComingSoon";
 
 // import { BrowserRouter } from 'react-router-dom';
